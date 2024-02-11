504a9481f658cf25fe88a7e633859b766474ed2b
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2024-02-11 20:12:17 +0100

    update provider test suite

--- src/test/java/com/example/atipera/adapter/impl/ProviderTestSuite.java
+++ src/test/java/com/example/atipera/adapter/impl/ProviderTestSuite.java
@@ -1,9 +1,13 @@
 package com.example.atipera.adapter.impl;
 
+import com.example.atipera.adapter.mapper.BranchMapper;
 import com.example.atipera.adapter.mapper.RepoInfoMapper;
 import com.example.atipera.adapter.web.GithubClient;
+import com.example.atipera.dto.BranchDto;
+import com.example.atipera.dto.CommitDto;
 import com.example.atipera.dto.OwnerDto;
 import com.example.atipera.dto.RepoInfoDto;
+import com.example.atipera.exception.IncorrectBranchException;
 import com.example.atipera.exception.IncorrectRepoInfoException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -13,8 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -22,7 +25,7 @@ import static org.mockito.Mockito.when;
 public class ProviderTestSuite {
 
     @Test
-    @DisplayName("Provider - exception when getRepoNames return null or empty array")
+    @DisplayName("Provider(getRepoByUsername) - exception when getRepoNames return null or empty array")
     void shouldThrowIncorrectRepoInfoExceptionWhenListIsNullOrEmpty() {
         //given
         var mockGithubClient = mock(GithubClient.class);
@@ -36,7 +39,7 @@ public class ProviderTestSuite {
     }
 
     @Test
-    @DisplayName("Provider - exception when getName or getOwner is null")
+    @DisplayName("Provider(getRepoByUsername) - exception when getName or getOwner is null")
     void shouldThrowIncorrectRepoInfoExceptionWhenPutIncorrectData() {
         //given
         RepoInfoDto repoWithNullName = new RepoInfoDto(null, new OwnerDto(), null);
@@ -56,8 +59,8 @@ public class ProviderTestSuite {
     }
 
     @Test
-    @DisplayName("Provider - should pass when put correct data")
-    void shouldPassWhenPutCorrectData() {
+    @DisplayName("Provider(getRepoByUsername) - should pass when put correct data")
+    void shouldPassGetRepoByUsernameWhenPutCorrectData() {
         //given
         RepoInfoDto repoInfoDto = new RepoInfoDto("name", new OwnerDto("login"), null);
 
@@ -69,5 +72,64 @@ public class ProviderTestSuite {
         var toTest = new ProviderImpl(mockGithubClient, mockRepoInfoMapper, null);
         //then
         Assertions.assertEquals(new ArrayList<>(), toTest.getRepoByUsername(anyString()));
+    }
+
+    @Test
+    @DisplayName("Provider(getBranchByUserNameAndRepoName) - exception when getBranchByUserNameAndRepoName return null or empty array")
+    void shouldThrowIncorrectBranchExceptionWhenListIsNullOrEmpty() {
+        //given
+        var mockGithubClient = mock(GithubClient.class);
+        when(mockGithubClient.getBranchByUserNameAndRepoName(eq("empty"),anyString())).thenReturn(new ArrayList<>());
+        when(mockGithubClient.getBranchByUserNameAndRepoName(eq("null"),anyString())).thenReturn(null);
+        //when
+        var toTest = new ProviderImpl(mockGithubClient, null, null);
+        //then
+        Assertions.assertThrows(IncorrectBranchException.class, () -> toTest.getBranchByUserNameAndRepoName("empty",""));
+        Assertions.assertThrows(IncorrectBranchException.class, () -> toTest.getBranchByUserNameAndRepoName("null",""));
+    }
+
+    @Test
+    @DisplayName("Provider(getBranchByUserNameAndRepoName) - exception when branchDto name or CommitDto is null")
+    void shouldThrowIncorrectBranchExceptionWhenPutIncorrectData() {
+        //given
+        BranchDto branchWithNullName = new BranchDto(null, new CommitDto("sha"));
+        BranchDto branchWithNullCommitDto = new BranchDto("name", null);
+        BranchDto branchWithNullNameAndNullCommitDto = new BranchDto(null, null);
+
+        var mockGithubClient = mock(GithubClient.class);
+        var mockBranchMapper = mock(BranchMapper.class);
+        when(mockGithubClient.getBranchByUserNameAndRepoName
+                (eq("branchWithNullName"), anyString())).thenReturn(List.of(branchWithNullName));
+        when(mockGithubClient.getBranchByUserNameAndRepoName
+                (eq("branchWithNullCommitDto"), anyString())).thenReturn(List.of(branchWithNullCommitDto));
+        when(mockGithubClient.getBranchByUserNameAndRepoName
+                (eq("branchWithNullNameAndNullCommitDto"), anyString())).thenReturn(List.of(branchWithNullNameAndNullCommitDto));
+
+        when(mockBranchMapper.fromDtoList(any())).thenReturn(new ArrayList<>());
+        //when
+        var toTest = new ProviderImpl(mockGithubClient, null, mockBranchMapper);
+        //then
+        Assertions.assertThrows(IncorrectBranchException.class, () ->
+                toTest.getBranchByUserNameAndRepoName("branchWithNullName", ""));
+        Assertions.assertThrows(IncorrectBranchException.class, () ->
+                toTest.getBranchByUserNameAndRepoName("branchWithNullCommitDto", ""));
+        Assertions.assertThrows(IncorrectBranchException.class, () ->
+                toTest.getBranchByUserNameAndRepoName("repoWithNullNameAndOwner", ""));
+    }
+
+    @Test
+    @DisplayName("Provider(getBranchByUserNameAndRepoName) - should pass when put correct data")
+    void shouldPassWhenPutCorrectData() {
+        //given
+        BranchDto branchDto = new BranchDto("name", new CommitDto("sha"));
+
+        var mockGithubClient = mock(GithubClient.class);
+        var mockBranchMapper = mock(BranchMapper.class);
+        when(mockGithubClient.getBranchByUserNameAndRepoName(anyString(), anyString())).thenReturn(List.of(branchDto));
+        when(mockBranchMapper.fromDtoList(any())).thenReturn(new ArrayList<>());
+        //when
+        var toTest = new ProviderImpl(mockGithubClient, null, mockBranchMapper);
+        //then
+        Assertions.assertEquals(new ArrayList<>(), toTest.getBranchByUserNameAndRepoName(anyString(), anyString()));
     }
 }
