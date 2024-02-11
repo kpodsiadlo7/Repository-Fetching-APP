f8bc61339053e106197f1f49996118909ccf25ef
kpodsiadlo7 <104767107+kpodsiadlo7@users.noreply.github.com>
2024-02-11 10:48:03 +0100

    commit mapper test suite

---
+++ src/test/java/com/example/atipera/adapter/CommitMapperTestSuite.java
@@ -0,0 +1,38 @@
+package com.example.atipera.adapter;
+
+import com.example.atipera.dto.CommitDto;
+import com.example.atipera.model.Commit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommitMapperTestSuite {
+
+    @Autowired
+    private CommitMapper commitMapper;
+
+    @Test
+    @DisplayName("CommitMapper - fromDto")
+    void mapToCommitFromCommitDto() {
+        //given
+        CommitDto commitDto = new CommitDto("testSha");
+        //when
+        Commit commit = commitMapper.fromDto(commitDto);
+        //then
+        Assertions.assertEquals("testSha", commit.getSha());
+    }
+
+    @Test
+    @DisplayName("CommitMapper - toDto")
+    void mapToCommitDtoFromCommit() {
+        //given
+        Commit commit = Commit.builder().sha("testSha").build();
+        //when
+        CommitDto commitDto = commitMapper.toDto(commit);
+        //then
+        Assertions.assertEquals("testSha", commitDto.getSha());
+    }
+}
